5613ceebdc541eb3395a7d29df4dce20d589dfc7
darie <darie@Airdel>
2020-06-21 16:33:06 -0600

    Test - f1 completo

--- Proyecto-VentaTec/src/Manejadores/Manejador_CerrarCaja.java
+++ Proyecto-VentaTec/src/Manejadores/Manejador_CerrarCaja.java
@@ -14,7 +14,7 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author LUISM
+ * @author LUISM----------------------------------------------------------------
  */
 public class Manejador_CerrarCaja {
     //---Declaracion de variables-----------//
@@ -40,6 +40,7 @@ public class Manejador_CerrarCaja {
         ICC.btn_CorteCaja.addActionListener(new ActionListener() {
             //-----Inicializa Modulo Cerrar Caja--------//
             public void actionPerformed(ActionEvent ae) {
+                try{
                 MCC.setS1(Integer.parseInt(ICC.spin_1.getValue() + ""));
                 MCC.setS10(Integer.parseInt(ICC.spin_10.getValue() + ""));
                 MCC.setS100(Integer.parseInt(ICC.spin_100.getValue() + ""));
@@ -59,6 +60,12 @@ public class Manejador_CerrarCaja {
                 ICC.lblTotal.setText("$"+MCC.getTotal());
                 ICC.lblCajaFondo.setText(MAC.getCajafondo() + "");
                 ICC.lblGanancias.setText(MCC.getGanancias() + "");
+                JOptionPane.showMessageDialog(ICC, "Corte realizado");
+                ICC.btn_CorteCaja.setEnabled(false);
+                }catch(Exception e){
+                    JOptionPane.showMessageDialog(ICC,e.getMessage());
+                }
+                
                 //-----Actualiza los label de Interfaz Cerrar Caja--------//
             }
         });
