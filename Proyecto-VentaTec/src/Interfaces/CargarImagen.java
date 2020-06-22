c8c35e229d3c9e601ed927a09f05a525fdcb3b38
user <user@SilverAcer>
2020-06-21 22:18:15 -0600

    BRAYAN 10:18 pm

--- Proyecto-VentaTec/src/Interfaces/CargarImagen.java
+++ Proyecto-VentaTec/src/Interfaces/CargarImagen.java
@@ -1,4 +1,4 @@
-/*
+/*hola enrique y luis
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
