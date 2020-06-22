0582a8afd34ce812e058b4283759e2d02c9438de
darie <darie@Airdel>
2020-06-22 03:02:02 -0600

    Actualizaciones de UI, Login y Principal

--- Proyecto-VentaTec/src/Interfaces/Interfaz_InicioSesion.java
+++ Proyecto-VentaTec/src/Interfaces/Interfaz_InicioSesion.java
@@ -48,6 +48,9 @@ public class Interfaz_InicioSesion extends javax.swing.JFrame {
         lblUser1 = new javax.swing.JLabel();
         btnLogin = new javax.swing.JButton();
         btn_Salir = new javax.swing.JButton();
+        lblUser2 = new javax.swing.JLabel();
+        lblUser3 = new javax.swing.JLabel();
+        lbl_Background = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Inicio de Sesion.");
@@ -71,10 +74,10 @@ public class Interfaz_InicioSesion extends javax.swing.JFrame {
         txtUser.setBounds(83, 158, 260, 25);
 
         lblPassword.setFont(new java.awt.Font("Montserrat", 1, 18)); // NOI18N
-        lblPassword.setForeground(new java.awt.Color(102, 102, 102));
+        lblPassword.setForeground(new java.awt.Color(204, 204, 204));
         lblPassword.setText("Contraseña:");
         pnLogin.add(lblPassword);
-        lblPassword.setBounds(83, 208, 120, 24);
+        lblPassword.setBounds(80, 210, 110, 24);
 
         txtPassword.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
         txtPassword.setForeground(new java.awt.Color(102, 102, 102));
@@ -97,26 +100,26 @@ public class Interfaz_InicioSesion extends javax.swing.JFrame {
         jSeparator2.setForeground(new java.awt.Color(102, 102, 102));
         jSeparator2.setDoubleBuffered(true);
         pnLogin.add(jSeparator2);
-        jSeparator2.setBounds(83, 188, 260, 10);
+        jSeparator2.setBounds(83, 188, 260, 20);
 
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Icon_Empresa_Transparente.png"))); // NOI18N
         pnLogin.add(jLabel1);
-        jLabel1.setBounds(151, 11, 102, 112);
+        jLabel1.setBounds(0, 0, 102, 112);
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Fotos/password2_45px.png"))); // NOI18N
         pnLogin.add(jLabel2);
-        jLabel2.setBounds(28, 225, 45, 45);
+        jLabel2.setBounds(30, 220, 45, 45);
 
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Fotos/UserName45px.png"))); // NOI18N
         pnLogin.add(jLabel3);
-        jLabel3.setBounds(32, 138, 45, 45);
+        jLabel3.setBounds(30, 150, 45, 45);
 
         lblUser1.setFont(new java.awt.Font("Montserrat", 1, 18)); // NOI18N
-        lblUser1.setForeground(new java.awt.Color(102, 102, 102));
+        lblUser1.setForeground(new java.awt.Color(204, 204, 204));
         lblUser1.setText("Usuario:");
         pnLogin.add(lblUser1);
-        lblUser1.setBounds(83, 128, 100, 24);
+        lblUser1.setBounds(80, 130, 120, 24);
 
         btnLogin.setBackground(new java.awt.Color(78, 193, 188));
         btnLogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Fotos/Login2.png"))); // NOI18N
@@ -128,7 +131,7 @@ public class Interfaz_InicioSesion extends javax.swing.JFrame {
             }
         });
         pnLogin.add(btnLogin);
-        btnLogin.setBounds(100, 300, 60, 40);
+        btnLogin.setBounds(160, 290, 100, 50);
 
         btn_Salir.setBackground(new java.awt.Color(255, 255, 255));
         btn_Salir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Iconos_InterfazPrincipal/bonito_1.png"))); // NOI18N
@@ -139,7 +142,23 @@ public class Interfaz_InicioSesion extends javax.swing.JFrame {
             }
         });
         pnLogin.add(btn_Salir);
-        btn_Salir.setBounds(260, 300, 50, 40);
+        btn_Salir.setBounds(330, 320, 50, 40);
+
+        lblUser2.setFont(new java.awt.Font("Montserrat", 1, 18)); // NOI18N
+        lblUser2.setForeground(new java.awt.Color(204, 204, 204));
+        lblUser2.setText("Ultramarinos");
+        pnLogin.add(lblUser2);
+        lblUser2.setBounds(100, 10, 120, 30);
+
+        lblUser3.setFont(new java.awt.Font("Montserrat", 2, 18)); // NOI18N
+        lblUser3.setForeground(new java.awt.Color(204, 204, 204));
+        lblUser3.setText("\"Los Ayala\"");
+        pnLogin.add(lblUser3);
+        lblUser3.setBounds(110, 40, 100, 30);
+
+        lbl_Background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/background_InicioSesion.png"))); // NOI18N
+        pnLogin.add(lbl_Background);
+        lbl_Background.setBounds(0, 0, 400, 370);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -229,6 +248,9 @@ public class Interfaz_InicioSesion extends javax.swing.JFrame {
     private javax.swing.JSeparator jSeparator2;
     private javax.swing.JLabel lblPassword;
     private javax.swing.JLabel lblUser1;
+    private javax.swing.JLabel lblUser2;
+    private javax.swing.JLabel lblUser3;
+    private javax.swing.JLabel lbl_Background;
     public javax.swing.JPanel pnLogin;
     public javax.swing.JPasswordField txtPassword;
     public javax.swing.JTextField txtUser;
