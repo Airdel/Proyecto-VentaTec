ab279fdb22fc0c4f07e4cdd30fada73db8eda14d
LUISM <LUISM@192.168.3.8>
2020-06-22 18:42:40 -0600

    Modificacion Proyecto Luis 22/6/2020 5:43

--- Proyecto-VentaTec/src/Manejadores/Manejador_Ventas.java
+++ Proyecto-VentaTec/src/Manejadores/Manejador_Ventas.java
@@ -70,6 +70,7 @@ public class Manejador_Ventas {
                                 if(!(A.equals(""))){
                                     B = A.split(",");
                                     B[2] = IV.txt_Cantidad.getText();
+                                    B[4] = Double.parseDouble(B[2]+"")*(Double.parseDouble(B[3]+"")*(1 - Double.parseDouble(B[5]+"")));
                                     DTM.addRow(B);
                                     
                                     int row = DTM.getRowCount();
@@ -117,6 +118,7 @@ public class Manejador_Ventas {
                                 if(!(A.equals(""))){
                                     B = A.split(",");
                                     B[2] = IV.txt_Cantidad.getText();
+                                    B[4] = Double.parseDouble(B[2]+"")*(Double.parseDouble(B[3]+"")*(1 - Double.parseDouble(B[5]+"")));
                                     DTM.addRow(B);
 
                                     int row = DTM.getRowCount();
@@ -151,7 +153,7 @@ public class Manejador_Ventas {
                 char c = ke.getKeyChar();
                 if (!(c >= 48 && c <= 57)) {
                     ke.consume();
-                } else if (IV.txt_Codigo.getText().length() == 4) {
+                } else if (IV.txt_Cantidad.getText().length() == 4) {
                     ke.consume();
                 }
             }
@@ -169,6 +171,7 @@ public class Manejador_Ventas {
                                 if(!(A.equals(""))){
                                     B = A.split(",");
                                     B[2] = IV.txt_Cantidad.getText();
+                                    B[4] = Double.parseDouble(B[2]+"")*(Double.parseDouble(B[3]+"")*(1 - Double.parseDouble(B[5]+"")));
                                     DTM.addRow(B);
                                     
                                     int row = DTM.getRowCount();
